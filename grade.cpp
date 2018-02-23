615bad90b556b1a6aa4a5348189093ed00b2303e
Du Park <dupark3@gmail.com>
2018-02-23 08:58:35 -0500

    First working version, but takes regular median, not optimistic median

--- grade.cpp
+++ grade.cpp
@@ -2,16 +2,15 @@
 #include <vector>
 
 #include "grade.h"
-#include "median.h"
+#include "optimisticMedian.h"
 #include "Student_info.h"
 
 double grade(double midterm, double final, const std::vector<double>& homework){
     if (homework.size() == 0)
         throw std::domain_error("No homework grades");
-    return grade(midterm, final, median(homework));
+    return grade(midterm, final, optimisticMedian(homework));
 }
 
 double grade(double midterm, double final, double homework){
     return (midterm * 0.2 + final * 0.4 + homework * 0.4);
 }
-
--- main.cpp
+++ main.cpp
@@ -3,55 +3,49 @@ Accelerated C++ Exercise 6-5
 Write an analysis function to call optimistic_median
 */
 
-#include <algorithm>
-#include <iomanip>
-#include <ios>
+#include <algorithm> // stable_partition
+#include <iomanip> // precision, setw
+#include <ios> // width
 #include <iostream>
 #include <list>
 #include <stdexcept>
 #include <string>
-#include <vector>
 
-#include "extract_fail.h"
 #include "grade.h"
+#include "passingGrade.h"
 #include "Student_info.h"
+
 
 typedef std::list<Student_info> container;
 typedef std::list<Student_info>::iterator iter;
 
 int main(){
-    // Uses List to contain student records for optimized insertion/deletion
-    container students;
-    // Student info struct contains data string "name" and grade double "finalGrade"
-    Student_info record;
+    container students; // Uses List to contain student records for optimized insertion/deletion
+    Student_info record; // Student info struct contains data string "name" and grade double "finalGrade"
 
     std::cout << "Enter student name, midterm, final, and all homework grades below." << std::endl;
 
     // Passes the console input and the temporary struct "record" to hold read values until it is appended to our vector
+    std::string::size_type maxLen = 0;
     while(read(std::cin, record)){
-        maxlen = std::max(maxlen, record.name.size());
+        maxLen = std::max(maxLen, record.name.size());
         students.push_back(record);
     }
 
     // Compare predicate uses "name" of student data, as defined in Student_info.cpp
     students.sort(compare);
 
-    // Extract students who failed and store in container
+    // Extract students who failed and store in new container
     iter seperator = stable_partition(students.begin(), students.end(), passingGrade);
-    container failStudents(separator, students.end());
-    students.erase(separator, students.end());
+    container failStudents(seperator, students.end());
+    students.erase(seperator, students.end());
 
-    // Resize the original vector by how many passing students have been copied to the front of the vector
-    students.resize(countPassing);
-    students.sort(compare);
-
-    // Print passing students in original container (fails have been extracted)
+    // Print passing students in original container
     std::cout << "PASSING STUDENTS:" << std::endl;
     for (iter i = students.begin(); i!= students.end(); ++i){
         // Print name then appropriate spaces (COULD USE WIDTH AND SETW??)
         std::cout << i->name
-                  << std::string(maxlen + 1 - (i->name).size(), ' ');
-
+                  << std::string(maxLen + 1 - (i->name).size(), ' ');
         std::streamsize prec = std::cout.precision();
         std::cout << std::setprecision(3)
                   << i->finalGrade
@@ -60,11 +54,10 @@ int main(){
     }
 
     std::cout << std::endl << "FAILING STUDENTS (under 60):" << std::endl;
-    for (iter i = fails.begin(); i!= fails.end(); ++i){
+    for (iter i = failStudents.begin(); i!= failStudents.end(); ++i){
         // Print name then appropriate spaces (COULD USE WIDTH AND SETW??)
         std::cout << i->name
-                  << std::string(maxlen + 1 - (i->name).size(), ' ');
-
+                  << std::string(maxLen + 1 - (i->name).size(), ' ');
         std::streamsize prec = std::cout.precision();
         std::cout << std::setprecision(3)
                   << i->finalGrade
